a08282bc599f888bdcc9ce725ec0bb11d49dbe42
ML-Laurane <laurane.mouronval@gmail.com>
2024-01-29 11:05:44 +0100

    Fix etape 2 (laurane et gauthier)

--- LibraryManager/LibraryManager/Program.cs
+++ LibraryManager/LibraryManager/Program.cs
@@ -1,4 +1,8 @@
-﻿public class Program
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Program
 {
     private static void Main(string[] args)
     {
