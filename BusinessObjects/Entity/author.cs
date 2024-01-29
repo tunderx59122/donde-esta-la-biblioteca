d6c98c791b7ce8949395bf7e116fae67bf0f1ac1
Evan <evan.labit2@gmail.com>
2024-01-29 09:53:03 +0100

    Added library and author entities

--- BusinessObjects/Entity/author.cs
+++ BusinessObjects/Entity/author.cs
@@ -2,6 +2,8 @@ namespace BusinessObjects.Entity
 {
     public class Author
     {
-
+        private int Id { get; set; }
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
     }
 }
--- BusinessObjects/Entity/library.cs
+++ BusinessObjects/Entity/library.cs
@@ -2,6 +2,8 @@ namespace BusinessObjects.Entity
 {
     public class Library
     {
-
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private string Adress { get; set; }
     }
 }
