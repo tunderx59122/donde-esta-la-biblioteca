200c16ae074a9cd354da9a632e262ffcea48db5f
Evan <evan.labit2@gmail.com>
2024-01-29 11:03:01 +0100

    Added author and library's repositories

---
+++ DataAccessLayer/Repository/AuthorRepository.cs
@@ -0,0 +1,17 @@
+﻿using BusinessObjects.Entity;
+
+namespace DataAccessLayer.Repository
+{
+    internal class AuthorRepository
+    {
+        public IEnumerable<Author> GetAll()
+        {
+            return [];
+        }
+
+        public Author Get(int id)
+        {
+            return new Author(1, "Victor", "Hugo");
+        }
+    }
+}
---
+++ DataAccessLayer/Repository/LibraryRepository.cs
@@ -0,0 +1,17 @@
+﻿using BusinessObjects.Entity;
+
+namespace DataAccessLayer.Repository
+{
+    internal class LibraryRepository
+    {
+        public IEnumerable<Library> GetAll()
+        {
+            return [];
+        }
+
+        public Library Get(int id)
+        {
+            return new Library(1, "Pablo", "12, rue");
+        }
+    }
+}
