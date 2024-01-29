0e11ed8b0ec6bd34b86b249eb2d3415a28b320de
Evan <evan.labit2@gmail.com>
2024-01-29 09:31:34 +0100

    added base project

---
+++ LibraryManager/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    private static IHost CreateHostBuilder(IConfigurationBuilder configuration)
+    {
+        return Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                // Configuration des services
+            })
+            .Build();
+    }
+}
