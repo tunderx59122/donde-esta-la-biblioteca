c7e517ec039e6d0178a76ea96b1fa85d9de3b0bb
Evan <evan.labit2@gmail.com>
2024-01-29 13:33:18 +0100

    Added Catalog's data access layer

---
+++ BusinessLayer/Catalog/CatalogManager.cs
@@ -0,0 +1,29 @@
+﻿using BusinessObjects.Entity;
+
+namespace BusinessLayer.Catalog
+{
+    internal class CatalogManager
+    {
+        public IEnumerable<Book> GetAll()
+        {
+            return BookRepository.GetAll();
+        }
+
+        public IEnumerable<Book> GetAll(string type)
+        {
+            IEnumerable<Book> books = [];
+
+            foreach (Book book in this.GetAll())
+            {
+                books.Add(book);
+            }
+
+            return books;
+        }
+
+        public Book FindBook(int id)
+        {
+            return BookRepository.Get(id);
+        }
+    }
+}
