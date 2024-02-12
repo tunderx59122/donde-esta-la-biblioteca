827f13a26541f127e43039ea07dc424bd12bdf82
ML-Laurane <laurane.mouronval@gmail.com>
2024-02-12 09:38:49 +0100

    update Program.cs

--- LibraryManager/LibraryManager/Program.cs
+++ LibraryManager/LibraryManager/Program.cs
@@ -1,15 +1,33 @@
 ﻿using BusinessLayer.Catalog;
 using DataAccessLayer.Contexts;
 using DataAccessLayer.Repository;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Services.Services;
+using System.Linq.Expressions;
 
 public class Program
 {
     private static void Main(string[] args)
     {
+        var configurtion = new ConfigurationBuilder();
         var host = CreateHostBuilder();
+        var catalogService = host.Services.GetRequiredService<ICatalogService>();
+        try
+        {
+            foreach (var book in catalogService.ShowCatalog())
+            {
+                Console.WriteLine($"{book.Name} by {book?.Author?.FirstName} / {book?.Author?.LastName}");
+            }
+
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception.Message);
+            Console.WriteLine(exception.StackTrace);
+
+        }
     }
 
     private static IHostBuilder CreateHostBuilder()
