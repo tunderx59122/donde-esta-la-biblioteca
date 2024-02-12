3e937222bbf273a8de7d2aaa4bded5a14adab67c
ML-Laurane <laurane.mouronval@gmail.com>
2024-02-12 13:34:24 +0100

    idauthor supprimé dans les exemples

--- LibraryManager/DataAccessLayer/Repository/BookRepository.cs
+++ LibraryManager/DataAccessLayer/Repository/BookRepository.cs
@@ -13,11 +13,11 @@ namespace DataAccessLayer.Repository
 
         public BookRepository()
         {
-            AddBook(new Book {Id = 1, Name = "Le conte de Monte Cristo", Pages = 900, Type = BookTypes.AVENTURE, Rate = 10, IdAuthor = 1 });
-            AddBook(new Book {Id = 2, Name = "Les trois mousquetaires", Pages = 300, Type = BookTypes.AVENTURE, Rate = 9, IdAuthor = 1 });
-            AddBook(new Book {Id = 3, Name = "Apprendre le Java mais pas sur l'île de Java", Pages = 900, Type = BookTypes.ENSEIGNEMENT, Rate = 8, IdAuthor = 2 });
-            AddBook(new Book {Id = 4, Name = "Le RC Lense, un club pas comme les autres", Pages = 900, Type = BookTypes.HISTOIRE, Rate = 5, IdAuthor = 4 });
-            AddBook(new Book { Id = 5, Name = "La RGPD, une protection contre l 'injustice", Pages = 900, Type = BookTypes.JURIDIQUE, Rate = 6, IdAuthor = 1 });
+            AddBook(new Book {Id = 1, Name = "Le conte de Monte Cristo", Pages = 900, Type = BookTypes.AVENTURE, Rate = 10});
+            AddBook(new Book {Id = 2, Name = "Les trois mousquetaires", Pages = 300, Type = BookTypes.AVENTURE, Rate = 9});
+            AddBook(new Book {Id = 3, Name = "Apprendre le Java mais pas sur l'île de Java", Pages = 900, Type = BookTypes.ENSEIGNEMENT, Rate = 8 });
+            AddBook(new Book {Id = 4, Name = "Le RC Lense, un club pas comme les autres", Pages = 900, Type = BookTypes.HISTOIRE, Rate = 5});
+            AddBook(new Book { Id = 5, Name = "La RGPD, une protection contre l 'injustice", Pages = 900, Type = BookTypes.JURIDIQUE, Rate = 6 });
 
         }
 
