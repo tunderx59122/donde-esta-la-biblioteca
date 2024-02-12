163ef9e5372b2c550ff36f40a81a312046fcfbd0
Gauthier Corion <43696669+MisterGranti67@users.noreply.github.com>
2024-02-12 15:16:43 +0100

    Tests

---
+++ LibraryManager/BusinessLayer.Test/CatalogManagerTest.cs
@@ -0,0 +1,12 @@
+namespace BusinessLayer.Test
+{
+    [TestClass]
+    public class CatalogManagerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
